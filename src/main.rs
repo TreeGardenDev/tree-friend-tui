d32a8ef85fa177cd658da9461eb3693126a0df82
Kyle Baumgarten <BaumDev@proton.me>
2023-08-27 13:28:12 -0400

    init dev

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend, 
+    Terminal,
+    layout::{Layout, Constraint, Direction},
+    widgets::{Widget,Block, Borders, Paragraph
+    }};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+const QUIT: &[KeyCode] = &[KeyCode::Char('c'), KeyCode::Char('q')];
+const CONFIG_PATH: &str = "config.json";
+
+fn main() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        //let size = f.size();
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(20),
+                    Constraint::Percentage(60),
+                    Constraint::Percentage(20),
+                ]
+                .as_ref(),
+            )
+            .split(f.size());
+        let block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
+
+    })?;
+
+    thread::sleep(Duration::from_millis(10000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
